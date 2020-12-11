38b9a5edc8650cc398462051dc3fddb55494a2d5
Devllench <lenya2012@mail.ru>
2020-12-11 03:55:31 +0300

    Create index html

---
+++ future_lav_app/resources/views/index.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.main')
+
+@section('header')
+    <header>
+        Page header 123
+    </header>
+@endsection
+
+@section('article')
+    <article>
+        Контент - основное содержимое страницы.
+    </article>
+@endsection
+
+@section('footer')
+    <footer>
+        Footer
+    </footer>
+@endsection
---
+++ future_lav_app/resources/views/layouts/main.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Future Test</title>
+    <link></link>
+</head>
+<body>
+@yield('header')
+@yield('article')
+@yield('footer')
+</body>
+</html>
